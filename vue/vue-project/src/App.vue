e10da75a4b5df5c4578386d300b2a87a5e627df1
FoggyMemories <diiioman@foxmail.com>
2023-08-02 10:14:27 +0800

    element

--- vue/vue-project/src/App.vue
+++ vue/vue-project/src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
-    <h1>{{ message}}</h1>
+    <!--<h1>{{ message}}</h1>-->
+    <element-view></element-view>
   </div>
 </template>
 <script>
+import ElementView from "@/views/element/ElementView.vue";
+
 export default {
+  components: {ElementView},
   data() {
     return {
       message: "Hello Vue"
     }
   },
-  methods:{}
+  methods: {}
 }
 </script>
 
