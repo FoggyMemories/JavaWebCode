b6e1415d373b671b0b0914a47a0a981ac2c4e72d
FoggyMemories <diiioman@foxmail.com>
2023-08-02 16:58:39 +0800

    element-tlias智能学习辅助系统

--- vue/vue-project/src/App.vue
+++ vue/vue-project/src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <div>
     <!--<h1>{{ message}}</h1>-->
-    <element-view></element-view>
+    <!--<element-view></element-view>-->
+
+    <!--  员工管理  -->
+    <emp-view></emp-view>
   </div>
 </template>
 <script>
-import ElementView from "@/views/element/ElementView.vue";
+/*import ElementView from "@/views/element/ElementView.vue";*/
+
+import EmpView from "@/views/tlias/EmpView.vue";
 
 export default {
-  components: {ElementView},
+  components: {
+    EmpView
+    /*ElementView*/},
   data() {
     return {
       message: "Hello Vue"
---
+++ vue/vue-project/src/views/tlias/EmpView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <el-container style="height: 700px; border: 1px solid #eee">
+      <el-header style="font-size: 40px; background-color: rgb(238, 241, 246);">tlias 智能学习辅助系统</el-header>
+      <el-container>
+        <el-aside width="200px">
+          <el-menu :default-openeds="['1', '3']">
+            <el-submenu index="1">
+              <template slot="title"><i class="el-icon-message"></i>系统信息管理</template>
+                <el-menu-item index="1-1">部门管理</el-menu-item>
+                <el-menu-item index="1-2">员工管理</el-menu-item>
+            </el-submenu>
+          </el-menu>
+        </el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+
+<script>
+export default {}
+</script>
+
+<style>
+
+</style>
