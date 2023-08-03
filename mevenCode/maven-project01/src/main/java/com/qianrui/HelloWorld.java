63342ddd239efd9db1aaf486c727d032c5b88711
FoggyMemories <diiioman@foxmail.com>
2023-08-03 22:51:04 +0800

    maven-依赖范围

--- mevenCode/maven-project01/src/main/java/com/qianrui/HelloWorld.java
+++ mevenCode/maven-project01/src/main/java/com/qianrui/HelloWorld.java
@@ -1,7 +1,11 @@
 package com.qianrui;
 
+/*import ch.qos.logback.classic.Logger;*/
+
 public class HelloWorld {
 	public static void main(String[] args) {
+		/*//验证依赖是否有效
+		Logger log; //<scope>test</scope> 此时依赖范围为test 主程序不可用*/
 		System.out.println("Hello Maven");
 	}
 }
---
+++ mevenCode/maven-project01/src/test/java/com/qianrui/test/DemoTest.java
@@ -0,0 +1,10 @@
+package com.qianrui.test;
+
+import ch.qos.logback.classic.Logger;
+
+public class DemoTest {
+	public void test() {
+		//验证依赖是否有效
+		Logger log;
+	}
+}
