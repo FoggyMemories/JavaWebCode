36d546e57f221e7de553337ab7c579faf4fe0d90
FoggyMemories <diiioman@foxmail.com>
2023-08-04 11:34:41 +0800

    maven-springboot-web-quickstart

---
+++ mevenCode/springboot-web-quickstart/src/main/java/com/qianrui/SpringbootWebQuickstartApplication.java
@@ -0,0 +1,13 @@
+package com.qianrui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebQuickstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebQuickstartApplication.class, args);
+	}
+
+}
